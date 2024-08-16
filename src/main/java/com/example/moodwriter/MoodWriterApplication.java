dff038ba7a6a137f47b31eac591d19d30e99c87f
nadoran78 <nadoran78@gmail.com>
2024-08-16 09:07:03 +0900

    first commit

---
+++ src/main/java/com/example/moodwriter/MoodWriterApplication.java
@@ -0,0 +1,13 @@
+package com.example.moodwriter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodWriterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MoodWriterApplication.class, args);
+  }
+
+}
