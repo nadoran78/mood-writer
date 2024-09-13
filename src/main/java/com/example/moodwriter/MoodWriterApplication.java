1a423b28ae13b2d24b3910cfee45b778c5cb1043
nadoran78 <nadoran78@gmail.com>
2024-09-13 14:06:28 +0900

    chore: test를 위해 security 적용 배제 (사유: 향후 security 처리 예정)

--- src/main/java/com/example/moodwriter/MoodWriterApplication.java
+++ src/main/java/com/example/moodwriter/MoodWriterApplication.java
@@ -2,8 +2,9 @@ package com.example.moodwriter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MoodWriterApplication {
 
   public static void main(String[] args) {
