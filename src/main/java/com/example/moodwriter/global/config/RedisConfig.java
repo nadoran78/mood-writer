4e4356e2711f51da7801472207ad3269158152da
nadoran78 <nadoran78@gmail.com>
2024-09-19 13:59:20 +0900

    fix: redis 밸류 serializer String 으로 변경

--- src/main/java/com/example/moodwriter/global/config/RedisConfig.java
+++ src/main/java/com/example/moodwriter/global/config/RedisConfig.java
@@ -24,11 +24,11 @@ public class RedisConfig {
   }
 
   @Bean
-  public RedisTemplate<String, Object> redisTemplate() {
-    RedisTemplate<String, Object> template = new RedisTemplate<>();
+  public RedisTemplate<String, String> redisTemplate() {
+    RedisTemplate<String, String> template = new RedisTemplate<>();
     template.setConnectionFactory(redisConnectionFactory());
     template.setKeySerializer(new StringRedisSerializer());
-    template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+    template.setValueSerializer(new StringRedisSerializer());
     return template;
   }
 
