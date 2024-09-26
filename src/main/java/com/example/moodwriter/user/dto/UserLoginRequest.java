2fad77e6ac02b4cc91efba91a368b8338e61d756
nadoran78 <nadoran78@gmail.com>
2024-09-26 22:38:59 +0900

    fix: request의 email, password에 @NotBlank 메시지 추가

--- src/main/java/com/example/moodwriter/user/dto/UserLoginRequest.java
+++ src/main/java/com/example/moodwriter/user/dto/UserLoginRequest.java
@@ -10,11 +10,11 @@ import lombok.Getter;
 @Builder
 public class UserLoginRequest {
 
-  @NotBlank
+  @NotBlank(message = "이메일은 반드시 입력해야 합니다.")
   @Pattern(regexp = RegexPattern.EMAIL, message = "올바른 이메일 형식이 아닙니다.")
   private String email;
 
-  @NotBlank
+  @NotBlank(message = "비밀번호는 반드시 입력해야 합니다.")
   @Pattern(regexp = RegexPattern.PASSWORD, message = "암호는 소문자, 대문자, 숫자, 특수문자 각각 최소 1개 이상을 포함하는 8자리 이상 20자리 이하여야 합니다.")
   private String password;
 
