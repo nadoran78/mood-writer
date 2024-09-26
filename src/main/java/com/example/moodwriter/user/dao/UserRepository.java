535ca0992a98be633dd132ac94136a03ff89f9e2
nadoran78 <nadoran78@gmail.com>
2024-09-26 18:34:27 +0900

    feat: login 기능 구현

--- src/main/java/com/example/moodwriter/user/dao/UserRepository.java
+++ src/main/java/com/example/moodwriter/user/dao/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.moodwriter.user.dao;
 
 import com.example.moodwriter.user.entity.User;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, UUID> {
   boolean existsByEmail(String email);
   User getByEmail(String email);
+
+  Optional<User> findByEmail(String email);
 }
---
+++ src/main/java/com/example/moodwriter/user/dto/UserLoginRequest.java
@@ -0,0 +1,18 @@
+package com.example.moodwriter.user.dto;
+
+import com.example.moodwriter.global.constant.RegexPattern;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserLoginRequest {
+
+  @Pattern(regexp = RegexPattern.EMAIL, message = "올바른 이메일 형식이 아닙니다.")
+  private String email;
+
+  @Pattern(regexp = RegexPattern.PASSWORD, message = "암호는 소문자, 대문자, 숫자, 특수문자 각각 최소 1개 이상을 포함하는 8자리 이상 20자리 이하여야 합니다.")
+  private String password;
+
+}
