8674c5bee07ff70e1062a8480f89bec76cef60f5
nadoran78 <nadoran78@gmail.com>
2024-09-30 22:19:49 +0900

    chore: Role admin 추가 (test 목적)

--- src/main/java/com/example/moodwriter/global/constant/Role.java
+++ src/main/java/com/example/moodwriter/global/constant/Role.java
@@ -1,6 +1,13 @@
 package com.example.moodwriter.global.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
-  ROLE_USER,
+  ROLE_USER("USER"), ROLE_ADMIN("ADMIN")
   ;
+
+  private final String role;
 }
