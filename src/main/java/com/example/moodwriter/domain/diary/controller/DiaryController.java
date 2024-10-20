c7fecfa7f54f848dab2738086e178668cb40cef8
nadoran78 <nadoran78@gmail.com>
2024-10-20 22:04:58 +0900

    feat: 일기 조회 기능 구현

--- src/main/java/com/example/moodwriter/domain/diary/controller/DiaryController.java
+++ src/main/java/com/example/moodwriter/domain/diary/controller/DiaryController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +61,14 @@ public class DiaryController {
       @PathVariable UUID diaryId,
       @AuthenticationPrincipal CustomUserDetails userDetails) {
     DiaryResponse response = diaryService.startEditingDiary(diaryId, userDetails.getId());
+    return ResponseEntity.ok(response);
+  }
+
+  @GetMapping("/{diaryId}")
+  public ResponseEntity<DiaryResponse> getDiary(
+      @PathVariable UUID diaryId,
+      @AuthenticationPrincipal CustomUserDetails userDetails) {
+    DiaryResponse response = diaryService.getDiary(diaryId, userDetails.getId());
     return ResponseEntity.ok(response);
   }
 
