9b325384638443f0d5e2e74a13a69fdf8f78485c
nadoran78 <nadoran78@gmail.com>
2024-10-30 13:56:01 +0900

    refactor: open ai 호출 기능 예외처리 추가

--- src/main/java/com/example/moodwriter/global/exception/code/ErrorCode.java
+++ src/main/java/com/example/moodwriter/global/exception/code/ErrorCode.java
@@ -34,7 +34,8 @@ public enum ErrorCode {
   ALREADY_DELETED_DIARY(HttpStatus.FORBIDDEN, "삭제된 일기입니다."),
   CONFLICT_DIARY_STATE(HttpStatus.CONFLICT, "일기가 유효한 상태가 아닙니다."),
 
-  OPEN_AI_RETURN_UNEXPECTED_RESPONSE(HttpStatus.INTERNAL_SERVER_ERROR, "Open AI API 호출 결과 예상치 못 한 응답코드가 반환되었습니다.")
+  OPEN_AI_RETURN_UNEXPECTED_RESPONSE(HttpStatus.INTERNAL_SERVER_ERROR, "Open AI API 호출 결과 예상치 못 한 응답코드가 반환되었습니다."),
+  FAIL_TO_CONNECT_WITH_OPEN_AI(HttpStatus.INTERNAL_SERVER_ERROR, "Open AI와 통신 중 오류가 발생하였습니다.")
 
   ;
 
--- src/main/java/com/example/moodwriter/global/openAI/service/OpenAIClient.java
+++ src/main/java/com/example/moodwriter/global/openAI/service/OpenAIClient.java
@@ -1,12 +1,16 @@
 package com.example.moodwriter.global.openAI.service;
 
+import static com.example.moodwriter.global.exception.code.ErrorCode.FAIL_TO_CONNECT_WITH_OPEN_AI;
+import static com.example.moodwriter.global.exception.code.ErrorCode.JSON_PARSE_ERROR;
 import static com.example.moodwriter.global.exception.code.ErrorCode.OPEN_AI_RETURN_UNEXPECTED_RESPONSE;
 
 import com.example.moodwriter.global.constant.OpenAIModel;
+import com.example.moodwriter.global.exception.CustomException;
 import com.example.moodwriter.global.openAI.dto.OpenAIRequest;
 import com.example.moodwriter.global.openAI.dto.OpenAIRequest.Message;
 import com.example.moodwriter.global.openAI.dto.OpenAIResponse;
 import com.example.moodwriter.global.openAI.exception.OpenAIException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.Collections;
@@ -34,7 +38,7 @@ public class OpenAIClient {
 
   private final String role = "user";
 
-  public OpenAIResponse callOpenAI(String diaryContent, OpenAIModel model) throws IOException {
+  public OpenAIResponse callOpenAI(String diaryContent, OpenAIModel model) {
 
     OpenAIRequest.Message message = Message.builder()
         .role(role)
@@ -46,7 +50,12 @@ public class OpenAIClient {
         .messages(Collections.singletonList(message))
         .build();
 
-    String jsonBody = objectMapper.writeValueAsString(request);
+    String jsonBody = null;
+    try {
+      jsonBody = objectMapper.writeValueAsString(request);
+    } catch (JsonProcessingException e) {
+      throw new CustomException(JSON_PARSE_ERROR);
+    }
 
     RequestBody requestBody = RequestBody.create(jsonBody,
         MediaType.parse("application/json"));
@@ -65,6 +74,8 @@ public class OpenAIClient {
       String responseBody = response.body().string();
 
       return objectMapper.readValue(responseBody, OpenAIResponse.class);
+    } catch (IOException e) {
+      throw new OpenAIException(FAIL_TO_CONNECT_WITH_OPEN_AI);
     }
 
   }
