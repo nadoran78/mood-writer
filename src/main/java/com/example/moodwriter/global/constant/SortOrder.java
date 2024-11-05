a63c64c244fd01f9a9cfcf77fb5ed589db84d2ef
nadoran78 <nadoran78@gmail.com>
2024-11-05 18:01:51 +0900

    chore: 불필요한 메서드 및 import 삭제

--- src/main/java/com/example/moodwriter/global/constant/SortOrder.java
+++ src/main/java/com/example/moodwriter/global/constant/SortOrder.java
@@ -1,18 +1,10 @@
 package com.example.moodwriter.global.constant;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum SortOrder {
   DESC, ASC;
 
 
   public static SortOrder forValue(String value) {
     return SortOrder.valueOf(value.toUpperCase());
-  }
-
-  @JsonValue
-  public String getValue() {
-    return this.name().toLowerCase();
   }
 }
