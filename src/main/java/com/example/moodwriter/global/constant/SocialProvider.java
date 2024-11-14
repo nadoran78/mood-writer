2a9a1dbde2a2883f2e0dfc3e33e6f5a9fbec0d73
nadoran78 <nadoran78@gmail.com>
2024-11-14 10:14:58 +0900

    refactor: SocialProvider enum jsonCreator 추가

--- src/main/java/com/example/moodwriter/global/constant/SocialProvider.java
+++ src/main/java/com/example/moodwriter/global/constant/SocialProvider.java
@@ -1,6 +1,18 @@
 package com.example.moodwriter.global.constant;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum SocialProvider {
   GOOGLE;
 
+  @JsonCreator
+  public static SocialProvider forValue(String value) {
+    return SocialProvider.valueOf(value.toUpperCase());
+  }
+
+  @JsonValue
+  public String toValue() {
+    return this.name().toLowerCase();
+  }
 }
