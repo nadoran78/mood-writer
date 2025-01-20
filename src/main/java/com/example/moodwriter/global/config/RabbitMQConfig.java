4b8ef441d5a6b8f1571228f787441f63a62e5091
nadoran78 <nadoran78@gmail.com>
2025-01-20 13:13:41 +0900

    refactor: rabbitMQ 설정 변경(메시지 직렬화 및 역직렬화 활성화)

--- src/main/java/com/example/moodwriter/global/config/RabbitMQConfig.java
+++ src/main/java/com/example/moodwriter/global/config/RabbitMQConfig.java
@@ -4,15 +4,42 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableRabbit
 public class RabbitMQConfig {
 
   public static final String NOTIFICATION_QUEUE = "notificationQueue";
   public static final String NOTIFICATION_EXCHANGE = "notificationExchange";
   public static final String NOTIFICATION_ROUTING_KEY = "notificationRoutingKey";
+
+  @Bean
+  public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+    RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    rabbitTemplate.setMessageConverter(jsonMessageConverter());
+    return rabbitTemplate;
+  }
+
+  @Bean
+  public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+    SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+    factory.setConnectionFactory(connectionFactory);
+    factory.setMessageConverter(jsonMessageConverter());
+    return factory;
+  }
+
+  @Bean
+  public Jackson2JsonMessageConverter jsonMessageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
 
   @Bean
   public Queue notificationQueue() {
