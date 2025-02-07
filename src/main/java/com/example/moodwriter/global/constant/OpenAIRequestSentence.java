cce44608e1c732dda03c3c3fac80dc2fe3f408b8
nadoran78 <nadoran78@gmail.com>
2025-02-07 16:38:41 +0900

    refactor: gpt에게 감정분석가 역할 부여

--- src/main/java/com/example/moodwriter/global/constant/OpenAIRequestSentence.java
+++ src/main/java/com/example/moodwriter/global/constant/OpenAIRequestSentence.java
@@ -17,7 +17,7 @@ public enum OpenAIRequestSentence {
           + "\n}"
   ),
   EMOTION_ANALYSIS(
-      "\n\n위 일기를 보고 감정분석을 상세하게 작성해주고, "
+      "\n\n위 일기를 보고 감정분석가가 되어 감정분석을 상세하게 작성해주고, "
           + "감정분석에 따른 적절한 조언을 상세히 작성해줘."
   )
 
