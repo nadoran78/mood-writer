620e146aa38d17d312c43ed42a6225c1217ffe82
nadoran78 <nadoran78@gmail.com>
2025-03-20 10:58:21 +0900

    chore: PropertyConfig 로컬에서만 사용하도록 수정

--- src/main/java/com/example/moodwriter/global/config/PropertyConfig.java
+++ src/main/java/com/example/moodwriter/global/config/PropertyConfig.java
@@ -1,10 +1,12 @@
 package com.example.moodwriter.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
 @Configuration
+@Profile("local")
 @PropertySources({
     @PropertySource("classpath:properties/env.properties")
 })
