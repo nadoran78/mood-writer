adeef4c426b3b73b7d3da31103e141680cd0d660
nadoran78 <nadoran78@gmail.com>
2025-04-11 18:31:21 +0900

    refactor: async 적용 조건부로 설정

--- src/main/java/com/example/moodwriter/global/config/AsyncConfig.java
+++ src/main/java/com/example/moodwriter/global/config/AsyncConfig.java
@@ -2,6 +2,7 @@ package com.example.moodwriter.global.config;
 
 import com.example.moodwriter.global.config.decorator.NotificationErrorHandlingTaskDecorator;
 import java.util.concurrent.Executor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,16 +12,23 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableAsync
 public class AsyncConfig {
 
+  @Value("${async.enabled:false}")
+  private boolean asyncEnabled;
+
   @Bean(name = "notificationTaskExecutor")
   public Executor notificationTaskExecutor() {
-    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(10);
-    executor.setMaxPoolSize(20);
-    executor.setQueueCapacity(50);
-    executor.setThreadNamePrefix("Notification-");
-    executor.setTaskDecorator(new NotificationErrorHandlingTaskDecorator());
-    executor.initialize();
-    return executor;
+    if (asyncEnabled) {
+      ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+      executor.setCorePoolSize(10);
+      executor.setMaxPoolSize(20);
+      executor.setQueueCapacity(50);
+      executor.setThreadNamePrefix("Notification-");
+      executor.setTaskDecorator(new NotificationErrorHandlingTaskDecorator());
+      executor.initialize();
+      return executor;
+    } else {
+      return Runnable::run;
+    }
   }
 
 }
